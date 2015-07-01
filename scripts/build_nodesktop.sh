e2ded23611d91b29005091c5570d4c538e8de0f9
Daniel Hurley <daniel.hurley@unimelb.edu.au>
2015-07-01 16:17:40 +1000

    First working version of vagrant-chaste.  Only runs 'scons core' tests though.

--- scripts/build_nodesktop.sh
+++ scripts/build_nodesktop.sh
@@ -1,35 +1,17 @@
 log=/vagrant/temp/install_chaste.log
 
-echo Getting code from the darcs repository and building it | tee -a $log
-cd ~/
-KIDNEY_REPO="/home/sbl/kidney_2013-10-09"
-if [ -d ${KIDNEY_REPO} ]; then
-    # If the directory already exists, delete it. It may be an incomplete copy
-    # (e.g., interrupted or unsuccessful download) and, if so, it will cause
-    # the newly-downloaded repository to be located in a different directory
-    # (most likely /home/sbl/kidney_2013-10-09_0).
-    rm -rf ${KIDNEY_REPO}
-fi
-darcs get --tag '1\.0-2013-1\+ajprenal' http://hub.darcs.net/rgm/kidney_2013-10-09
-cd kidney_2013-10-09
-eval `opam config env`
-make
-
-#echo Copy in the solutions JSON file for testModel
-# This is a workaround until we have recomputed solutions
-#cp /vagrant/temp/data/solutions.json /home/sbl/kidney_2013-10-09/models/testModel/solutions.json
+echo Building Chaste | tee -a $log
+cd $HOME
+tar -jxf /usr/src/chaste-source.tar.bz2
+cd chaste-source
+scons core
 
 
-echo Getting build info | tee -a $log
-echo This part is commented out until I figure out how to do it with darcs | tee -a $log
+#echo Getting build info | tee -a $log
+#We aren't doing this right now, but if we did it'd be like the below
 #git --git-dir ~/budden2015treeome/.git log --max-count=1 --format=format:"Last Commit: %h%nAuthor: %an%nCommit Date: %ad%n" > /vagrant/temp/build_info.txt
-printf 'Last commit:\n' > /vagrant/temp/build_info.txt
-darcs log --last 1 | awk '{print $1,$2,$3,$4,$5,$6}' >> /vagrant/temp/build_info.txt
 printf '\nEnvironment built at ' >> /vagrant/temp/build_info.txt
 date >> /vagrant/temp/build_info.txt
 
-# Copy the simulation scripts to a folder on the Desktop
-DEST_DIR=/home/sbl/Desktop/run
-sudo mkdir ${DEST_DIR}
-sudo cp /vagrant/temp/data/make_Figure*.sh ${DEST_DIR}
-sudo chmod 777 ${DEST_DIR}/*.sh
+# Copy some scripts to a folder on the Desktop
+# We aren't doing this either
--- scripts/install_chaste_required_packages.sh
+++ scripts/install_chaste_required_packages.sh
@@ -1,54 +1,18 @@
-log=/vagrant/temp/install_chaste_kidney.log
+log=/vagrant/temp/install_chaste.log
 
-echo Installing packages required for chaste | tee -a $log
+echo Installing packages required for Chaste | tee -a $log
 
-#wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-#sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu precise-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
-
-echo Adding keys and the Rstudio CRAN details for latest packages | tee -a $log
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list.d/getdeb.list'
-
-echo Adding keys for the latest OCaml and OPAM package | tee -a $log
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 61707B09
-sudo sh -c 'echo "deb http://ppa.launchpad.net/avsm/ppa/ubuntu trusty main" >> /etc/apt/sources.list.d/getdeb.list'
-sudo sh -c 'echo "deb-src http://ppa.launchpad.net/avsm/ppa/ubuntu trusty main " >> /etc/apt/sources.list.d/getdeb.list'
+echo Adding the Chaste package keys | tee -a $log
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 422C4D99
+sudo sh -c 'echo "deb http://www.cs.ox.ac.uk/chaste/ubuntu trusty/" >> /etc/apt/sources.list.d/getdeb.list'
 
 echo Updating after adding all those keys | tee -a $log
 sudo apt-get -y update
 
-echo Installing Mercurial which should really be in the base | tee -a $log
-sudo apt-get install -y mercurial
+echo Installing Chaste packages
+sudo apt-get install -y chaste-source
 
-echo Installing R | tee -a $log
-sudo apt-get -y install r-base r-base-dev evince libcairo2-dev libxt-dev r-cran-ggplot2
-
-echo Giving everyone all permissions to /usr/local/lib | tee -a $log
-#This is a copout to get compilation working
-sudo chmod -R 777 /usr/local/lib
-
-echo Installing R packages | tee -a $log
-Rscript /vagrant/temp/data/install_packages.R
-
-echo Installing Darcs | tee -a $log
-#Installing it quietly to prevent difficult questions about
-#postfix setup so I hope this works
-sudo apt-get install -qq -y --no-install-recommends darcs
-
-echo Installing opam | tee -a $log
-sudo apt-get -y install opam
-opam init -y
-opam install -y lacaml batteries atdgen ounit 
-eval `opam config env`
-
-echo Installing and setting up the SIunits library | tee -a $log
-# download the source for SIunits version 0.1
-darcs get http://hub.darcs.net/rgm/SIunits --tag 0.1
-# change into the source directory
-cd SIunits
-# install the SIunits library
-make install
-
+echo Tidying up by running autoremove | tee -a $log
 sudo apt-get -y autoremove
 
 echo Completed package installation | tee -a $log
