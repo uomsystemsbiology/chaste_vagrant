99534dfb253d29bc2a42fe22a1f5064b912574de
Daniel Hurley <daniel.hurley@unimelb.edu.au>
2015-07-01 16:49:44 +1000

    Fixed a couple of typos in background text.

--- scripts/build_nodesktop.sh
+++ scripts/build_nodesktop.sh
@@ -6,7 +6,6 @@ tar -jxf /usr/src/chaste-source.tar.bz2
 cd chaste-source
 scons core
 
-
 #echo Getting build info | tee -a $log
 #We aren't doing this right now, but if we did it'd be like the below
 #git --git-dir ~/budden2015treeome/.git log --max-count=1 --format=format:"Last Commit: %h%nAuthor: %an%nCommit Date: %ad%n" > /vagrant/temp/build_info.txt
